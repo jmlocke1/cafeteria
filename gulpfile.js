2dbd365998e88f478058f379d02b1dd25a3fe0f3
Josemi Xeon <worldcode.info@gmail.com>
2023-11-19 13:03:10 +0100

    Agregando un watch para compilar automáticamente cuando se produzcan cambios

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
-const { src, dest } = require('gulp');
+const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css( done ) {
 	// Compilar sass
 	// pasos: 1 - Identificar archivo, 2- Compilarla, 3 - Guardar el .css
 	src('src/scss/app.scss')
+		.pipe( plumber() )
 		.pipe( sass() )
 		.pipe( dest('build/css') )
 	done();
 }
 
+function dev( done ) {
+	watch( 'src/scss/app.scss', css );
+	done();
+}
 exports.css = css;
+exports.dev = dev;
+exports.default = dev;
